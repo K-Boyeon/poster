6580dbb1f63043c76ae0beb18df761a17e60c319
K-Violet <eyhc1212@naver.com>
2021-11-18 22:31:48 +0900

    animation 추가

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 $(function(){
+    var h = 0;
+    var t = 0;
+    
+    setTimeout(function(){
+        $("html, body").animate({scrollTop : 0}, 700, function(){
+            $("#page1 .title").addClass("on");
+        });
+    }, 150);
+
     $(window).resize(function(){ //이벤트 헨들링의 첫 시작이 setTimeout -> 이벤트 헨들링을 조종해주는 역할
 		h = $(window).height();
 		$("#page").css({height:h});//css속성을 줌으로써 자동적으로 px단위를 인식함
 	});
 	$(window).trigger("resize"); //강제로 실행(trigger)
 
-    setTimeout(function(){
-        $("html, body").animate({scrollTop : 0}, 600, function(){
-            $(".title").addClass("on");
+    $(window).scroll(function(){
+        t = $(this).scrollTop();
+        console.log(t);
+        
+        if(t <= $("#page2").offset().top){
+            $("#page2 .image").addClass("active");
+            $("#page2 .text").addClass("active");
+            $("#page2").addClass("show");
+        }
+    });
 
-            $(window).scroll(function(){
-                const t = $(this).scrollTop();
-                console.log(t);
-                
-                if(t <= $("#page2").offset().top){
-                    $(".image").addClass("active");
-                    $(".has-ani").addClass("animate-in");
-                }
-            });
-        });
-    }, 150);
+    const thisYear = document.querySelector(".this-year");
+    thisYear.textContent = new Date().getFullYear();
 });
 
